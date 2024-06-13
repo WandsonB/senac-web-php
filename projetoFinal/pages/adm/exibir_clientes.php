9f61598df8cd95dd580f125aaa9623d50032800f
WandsonB <108149915+WandsonB@users.noreply.github.com>
2024-06-12 21:00:36 -0300

    0.0.10
    Criação da pagina de cadastro dos serviços e refatorado a estrutura de pastas

---
+++ projetoFinal/pages/adm/exibir_clientes.php
@@ -0,0 +1,19 @@
+<?php
+    require_once "../php/conexao/connect.php";
+
+    if(isset($_GET['id'])) { //verifica se o campo id nao esta vazio usando o metodo GET
+        $id = $_GET['id'];
+        $sql = "SELECT * FROM usuarios WHERE id = $id";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0){
+            $row = $result->fetch_assoc();
+            $nome = $row['nome'];
+            $email = $row['email'];
+        } else {
+            echo 'Usuario não encontrado'; 
+        }
+
+        $conn->close();
+    }
+?>
---
+++ projetoFinal/php/conexao/connect.php
@@ -0,0 +1,15 @@
+<?php
+    // PRIMEIRO CRIAR AS VARIAVEIS QUE RECEBERAM AS INFORMAÇÕES DE ACESSO AO BD
+        $servername = "localhost"; 
+        $username = "Admin"; 
+        $password = "https://algo.muito.bom/"; 
+        $database = "atendimento";
+    
+    // COMANDO PARA FAZER A CONEXAO POR MEIO DA FERRAMENTA MYSQLI, PDO, ODBC, SQLSRV.
+    $connect = mysqli($$servername, $username, $password, $database)
+
+    //  VERIFICAR CONEXÃO
+    if ($connect->connect_error) {
+        die("Conexão falhou: " . $connect->connect_error);
+    }
+?>
